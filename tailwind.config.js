b458820b51891d538a58b0441f72627c11da14df
FlavioGP <flavio.gpinheiro@hotmail.com>
2025-01-14 10:35:52 -0300

    chore: add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js, jsx, ts, tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+
+        "neutral-0": "hsl(0, 0%, 100%)",
+        "neutral-300": "hsl(252, 6%, 83%)",
+        "neutral-500": "hsl(245, 15%, 58%)",
+        "neutral-700": "hsl(245, 19%, 35%)",
+        "neutral-900": "hsl(248, 70%, 10%)",
+        "Orange-500": "hsl(7, 88%, 67%)",
+        "Orange-700": "hsl(7, 71%, 60%)",
+        "Gradient-text": "hsl(7, 86%, 67%) to hsl(0, 0%, 100%)",
+      }
+    },
   },
   plugins: [],
 }
