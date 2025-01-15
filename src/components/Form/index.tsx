8044e2a61e43c46f81530d454fccbe99bb281f62
FlavioGP <flavio.gpinheiro@hotmail.com>
2025-01-15 10:50:34 -0300

    feat: add drag and drop for user image

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
 import UploadIcon from "../../assets/images/icon-upload.svg";
 
+type UserImageType = {
+  name: string;
+  size: number;
+  preview: string;
+};
+
 const Form = () => {
+  const [userImage, setUserImage] = useState<UserImageType | null>(null);
+
   const inputStyles = `rounded-xl border border-neutral-500 bg-neutral-300/10 text-nneutral-0`;
+
+  const dragEvents = {
+    onDragEnter: (e: React.DragEvent) => e.preventDefault(),
+    onDragLeave: (e: React.DragEvent) => e.preventDefault(),
+    onDragOver: (e: React.DragEvent) => e.preventDefault(),
+    onDrop: (e: React.DragEvent) => {
+      e.preventDefault();
+      const file = Array.from(e.dataTransfer.files);
+      console.log(file);
+      if (file.length !== 1) throw new Error("Insira apenas uma imagem");
+      console.log("ok 1 img");
+
+      const { name, size } = file[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file[0]);
+      reader.onloadend = () => {
+        const preview = reader.result;
+        if (typeof preview !== "string") return;
+        const image = { name, size, preview };
+        setUserImage(image);
+      };
+    },
+  };
 
   return (
     <form className="flex w-full flex-col gap-4 p-6">
@@ -10,6 +42,7 @@ const Form = () => {
         <label
           htmlFor="avatar"
           className={`${inputStyles} flex cursor-pointer flex-col items-center gap-4 border-dashed py-5`}
+          {...dragEvents}
         >
           <input type="file" name="avatar" id="avatar" className="h-0 w-0" />
           <div className="w-max rounded-xl border border-neutral-500 bg-neutral-500/30 p-3 shadow-md">
